99af17626f97f0e21b05ab631564d6e3896cf39a
Ngoneba01 <ndeye-ngone-dupuy.ba@epitech.digital>
2023-04-22 21:58:49 +0200

    header completed

--- my_rotten_tomatoes/components/Header.tsx
+++ my_rotten_tomatoes/components/Header.tsx
@@ -1,23 +1,31 @@
+import {SearchIcon, UserIcon} from '@heroicons/react/solid'
+
 function Header() {
     return <header>
             <div className="flex items-center space-x-2 md:space-x-10">
-                <img src="https://www.rottentomatoes.com/assets/pizza-pie/images/rottentomatoes_logo_40.336d6fe66ff.png" 
+                <img src="https://images.fandango.com/cms/assets/bf631b80-bf47-11ed-a868-adceb8892ad3--rt25-logo-mainnav-322x100.png" 
                 width={200}
                 height={200}
               className="cursor-pointer object-contain"
                 />
 
                 <ul className="flex justify-around space-x-20">
-                    <li>Movies </li>
-                    <li>Tv Shows</li>
-                    <li>Movie Trivia </li>
-                    <li>News </li>
-                    <li>Showtimes </li>
+                    <li className="headerLink">Movies </li>
+                    <li className="headerLink">Tv Shows</li>
+                    <li className="headerLink">Movie Trivia</li>
+                    <li className="headerLink">News</li>
+                    <li className="headerLink">Showtimes</li>
                 </ul>
+
+
             </div>
+               
 
-
-            <div> </div>
+            <div className='flex items-center space-x-4 font-light '>
+             <SearchIcon className='h-6 w-6 fill-primary'/>
+             <UserIcon className='h-6 w-6 fill-primary'/>
+                
+            </div>
     </header>
 }
 

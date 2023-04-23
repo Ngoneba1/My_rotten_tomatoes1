2ace9bcf04418587a0d92c8e6d3cb600592230ab
Ngoneba01 <ndeye-ngone-dupuy.ba@epitech.digital>
2023-04-23 21:40:23 +0200

    avancement

--- my_rotten_tomatoes/components/Banner.tsx
+++ my_rotten_tomatoes/components/Banner.tsx
@@ -1,5 +1,54 @@
-function Banner() {
-    return <div> Banner </div>
+import Image from 'next/image';
+import { Movie } from "../typings"
+import { useEffect, useState} from 'react'
+import { baseUrl } from '@/Constants/movie'
+
+
+
+
+interface Props{
+    netflixOriginals: Movie[]
+}
+
+function Banner({netflixOriginals}:Props) {
+    const [movie, setMovie] = useState < Movie | null>(null)
+    useEffect(() => {
+        setMovie(
+          netflixOriginals?.[Math.floor(Math.random() * netflixOriginals?.length ?? 0)]
+
+        )
+      }, [netflixOriginals])
+
+      console.log(movie)
+
+    return (
+      <div>
+        <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+            src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path || ''}`}
+            layout="fill"
+            objectFit="cover"
+            alt={''}
+/>
+ 
+          
+        
+        
+        </div>    
+
+
+
+
+      </div>
+
+
+
+           
+    )
 }
 
 export default Banner
+
+
+
+
--- my_rotten_tomatoes/next.config.js
+++ my_rotten_tomatoes/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ["m.media-amazon.com"],
+  domains:['image.tmdb.org'],
   },
-};
+  }
